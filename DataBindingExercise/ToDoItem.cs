4afb542eea39a0eab858f7807795c39dca748186
Nathan Bean <nhbean@ksu.edu>
2020-10-04 10:06:02 -0500

    Initial commit

---
+++ DataBindingExercise/ToDoItem.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataBindingExercise
+{
+    /// <summary>
+    /// Represents a single item in a collection of ToDos
+    /// </summary>
+    public class ToDoItem
+    {
+        /// <summary>
+        /// The task this ToDoItem embodies
+        /// </summary>
+        public string Task { get; set; }
+
+        /// <summary>
+        /// Indicates if this task has been completed
+        /// </summary>
+        public bool Complete { get; set; }
+
+        /// <summary>
+        /// Constructs a new instance of ToDoItem with the supplied <paramref name="task"/>
+        /// </summary>
+        /// <param name="task"></param>
+        public ToDoItem(string task)
+        {
+            Task = task;
+        }
+
+        /// <summary>
+        /// Displays the ToDoItem as a string
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            string status = Complete ? "Complete" : "Incomplete";
+            return $"{Task} - {status}";
+        }
+    }
+}
---
+++ DataBindingExercise/ToDoListControl.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataBindingExercise
+{
+    /// <summary>
+    /// Interaction logic for ToDoListControl.xaml
+    /// </summary>
+    public partial class ToDoListControl : UserControl
+    {
+        public ToDoListControl()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Adds a task to the ToDoList
+        /// </summary>
+        /// <param name="sender">The button that sent the event</param>
+        /// <param name="e">The event args</param>
+        void OnAddTaskClick(object sender, RoutedEventArgs e)
+        {
+            var task = new ToDoItem(newTaskTextBox.Text);
+            taskListView.Items.Add(task);
+            newTaskTextBox.Clear();
+        }
+    }
+}
